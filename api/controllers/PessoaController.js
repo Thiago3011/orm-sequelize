1b9df585e1c0c302a4f31b30e2a2bb7d704bcd14
Thiago3011 <thiago.silva1001@etec.sp.gov.br>
2022-12-05 19:24:36 -0300

    CRUD com sequelize

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -10,6 +10,61 @@ class PessoaController {
             return res.status(500).json(erro.message)
         }
     }
+
+    static async pegaUmaPessoa(req, res) {
+        const { id } = req.params
+
+        try {
+            const Umapessoa = await database.Pessoas.findOne({ where: { id: Number(id) } })
+
+            return res.status(200).json(Umapessoa)
+        } catch (erro) {
+            return res.status(500).json(erro.message)
+        }
+    }
+
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body
+
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+
+            return res.status(200).json(novaPessoaCriada)
+
+        } catch (error) {
+            return res.status(500).json(erro.message)
+        }
+    }
+
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params
+        const novasInfos = req.body
+
+        try {
+
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id) } }) //update nao retorna direto, retorna apenas 0 ou 1
+
+            const pessoaEditada = await database.Pessoas.findOne({ where: { id: Number(id) } })
+
+            return res.status(200).json(pessoaEditada)
+
+        } catch (erro) {
+            return res.status(500).json(erro.message)
+        }
+    }
+
+    static async deletaPessoa(req, res) {
+        const { id } = req.params
+
+        try {
+
+            await database.Pessoas.destroy({ where: { id: Number(id) } })
+
+            return res.status(200).json({message: `id ${id} deletado com sucesso!`})
+        } catch (erro) {
+            return res.status(500).json(erro.message)
+        }
+    }
 }
 
 module.exports = PessoaController
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,10 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.deletaPessoa)
+
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 
 module.exports = router
